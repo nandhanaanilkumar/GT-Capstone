431416aa78c5f1a868d523da4617f5fa32710266
nandhanaanilkumar <nandhanaanil2482@gmail.com>
2025-11-24 01:13:12 +0530

    updated

--- app/myntra.py
+++ app/myntra.py
@@ -126,7 +126,7 @@ page = st.sidebar.radio(
 col1, col2 = st.columns([1, 8])
 
 with col1:
-    st.image("myntra_logo-removebg-preview.png", width=80)
+    st.image("app/assets/myntra_logo-removebg-preview.png", width=80)
 
 with col2:
     st.markdown(
